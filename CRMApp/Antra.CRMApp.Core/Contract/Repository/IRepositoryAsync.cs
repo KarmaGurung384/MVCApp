b756a89e2f22b72c60dac5c09451a3cafbebf690
KarmaGurung384 <101598017+KarmaGurung384@users.noreply.github.com>
2022-06-14 16:25:31 -0500

    again

---
+++ CRMApp/Antra.CRMApp.Core/Contract/Repository/IRepositoryAsync.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antra.CRMApp.Core.Contract.Repository
+{
+    public interface IRepositoryAsync<T> where T : class
+    {
+        Task<int> InsertAsyn(T entity);
+        Task<int> UpdateAsyn(T entity);
+        Task<int> DeleteAsyn(int id);
+        Task<T> GetByIdAsync (int id);
+        Task<IEnumerable<T>> GetAllAsync();
+
+
+    }
+}
---
+++ CRMApp/Antra.CRMApp.Infrastructure/Repository/RegionRepositoryAsync.cs
@@ -0,0 +1,33 @@
+﻿using Antra.CRMApp.Core.Contract.Repository;
+using Antra.CRMApp.Core.Entity;
+using Antra.CRMApp.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antra.CRMApp.Infrastructure.Repository
+{
+    public class RegionRepositoryAsync : BaseRepository<Region>, IRegionRepositoryAsync
+    {
+        public RegionRepositoryAsync(CrmDbContext _dbContext) : base(_dbContext)
+        { 
+        }
+
+        public Task<int> DeleteAsyn(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertAsyn(Region entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsyn(Region entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
